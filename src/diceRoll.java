7e07d6c7009d8d9c3d09c49b0ce502ee090f519d
Bill Spencer <badgley@gmail.com>
2016-04-12 09:12:53 -0400

    new commit

--- src/diceRoll.java
+++ src/diceRoll.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class diceRoll {
 	static Random random = new Random();
@@ -6,10 +7,9 @@ public class diceRoll {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Grand Circus Casino!");
 		Scanner scan = new Scanner(System.in);
-		String input;
 		int sides, roll1, roll2;
 		System.out.print("Roll the dice? (y/n): ");
-		input = scan.next();
+		String input = scan.nextLine();
 		while (input.equalsIgnoreCase("y")) {						//creates loop for game as long as user agrees
 			System.out.print("How many sides do the dice have?: "); //asks for input for number of sides
 			sides = scan.nextInt();									//applies sides to use in rollDice method
@@ -20,11 +20,11 @@ public class diceRoll {
 			System.out.println(roll1);
 			System.out.println(roll2);								//prints the value of rolls and continues 
 																	//if value to print if value is 'craps'
-			if ((roll1 + roll2) == 2) {								//'snake eyes' or 'box cars'
+			if (roll1 + roll2 == 2) {								//'snake eyes' or 'box cars'
 				System.out.println("SNAKE EYES!");
 			} else if (roll1 == 6 && roll2 == 6) {
 				System.out.println("BOX CARS");
-			} else if ((roll1 + roll2) == 11 || (roll1 + roll2) == 7) {
+			} else if (roll1 + roll2 == 11 || roll1 + roll2 == 7) {
 				System.out.println("CRAPS!");
 			}
 
@@ -33,14 +33,10 @@ public class diceRoll {
 		}
 		System.out.println("Thank you for playing!");
 		scan.close();
-
 	}
 
-
-
 	public static int rollDice(int x) {
-		int dice;
-		dice = random.nextInt(x) + 1;
+		int dice = random.nextInt(x) + 1;
 		return dice;
 	}
 }
